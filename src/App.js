2ef6c1fd01ec0b4dcad7b790bf4046b86305969c
soflil <134602526+soflil@users.noreply.github.com>
2023-10-20 16:49:30 +0200

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        <a
-          href="https://github.com/soflil/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>
-        , by Sophie Schou
-      </footer>
+      <div className="container">
+        <footer>
+          <a
+            href="https://github.com/soflil/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>
+          , by Sophie Schou
+        </footer>
+      </div>
     </div>
   );
 }
