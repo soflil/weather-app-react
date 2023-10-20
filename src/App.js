61eb745d4358d77f46ecbd8d4f9c20b1f1005790
soflil <134602526+soflil@users.noreply.github.com>
2023-10-20 16:25:00 +0200

    Created a footer in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <footer>
+        <a
+          href="https://github.com/soflil/weather-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        , by Sophie Schou
+      </footer>
+    </div>
+  );
 }
