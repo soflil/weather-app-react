fc0b2770e3be5b8a7362144097fb8f9136d48b45
soflil <134602526+soflil@users.noreply.github.com>
2023-10-21 11:09:30 +0200

    Added API integration.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Oslo" />
         <footer>
           <a
             href="https://github.com/soflil/weather-app-react"
--- src/Weather.js
+++ src/Weather.js
@@ -1,52 +1,76 @@
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ active: false });
+
+  function handleData(response) {
+    setWeatherData({
+      active: true,
+      city: response.data.name,
+      date: "",
+      description: response.data.weather[0].description,
+      icon: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      temperature: Math.round(response.data.main.temp),
+      humidity: response.data.main.humidity,
+      windspeed: response.data.wind.speed,
+    });
+  }
+
+  if (weatherData.active) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="text"
+                placeholder="Enter city"
+                autoFocus="on"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-light btn-outline-secondary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>Oslo</h1>
+        <ul>
+          <li>Wednessday 19:00</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="text"
-              placeholder="Enter city"
-              autoFocus="on"
-              className="form-control"
-            />
+          <div className="col-6">
+            <div className="icon-temp-display">
+              <img
+                src={weatherData.icon}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <span className="temperature">{weatherData.temperature}</span>
+              <span className="unit">°C</span>
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-light btn-outline-secondary w-100"
-            />
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}</li>
+              <li>Windspeed: {weatherData.windspeed} km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <h1>Oslo</h1>
-      <ul>
-        <li>Wednessday 19:30</li>
-        <li>Mostly Cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div className="icon-temp-display">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Cloudy"
-              className="float-left"
-            />
-            <span className="temperature">6</span>
-            <span className="unit">°C</span>
-          </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: 30%</li>
-            <li>Windspeed: 4 km/h</li>
-            <li>Percipitation: 15%</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "57b2c40fdae71a6ba41d72685e3226e2";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleData);
+
+    return "Loading weather data...";
+  }
 }
